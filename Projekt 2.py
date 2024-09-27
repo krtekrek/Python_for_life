916efa9e4c9c7c23b98372559e9ee2d99288089e
krtekrek <krtekrek@seznam.cz>
2024-09-27 11:37:31 +0200

    Projekt 1 vole

---
+++ Projekt 2.py
@@ -0,0 +1,10 @@
+print (5 * 5)
+num = 150
+print (num)
+num2 = 50
+result = num - num2
+print (result)
+
+#Write a Python code to iterate the first 10 numbers, and in each
+# iteration, print the sum of the current and previous number.
+
