6d40f9a3d292f8540d1f6c1c0b406e212d6ba310
krtekrek <krtekrek@seznam.cz>
2024-10-09 10:14:37 +0200

    Projekt

--- Projekt 2.py
+++ Projekt 2.py
@@ -1,10 +1,15 @@
+print ("5*5 =",5*5)
 print (5 * 5)
 num = 150
+
 print (num)
 num2 = 50
 result = num / num2
 print (result)
 
+a = 2
+b = 3
+print (a+b)
 #Write a Python code to iterate the first 10 numbers, and in each
 # iteration, print the sum of the current and previous number.
 
